f847d3eb97407b69216b7a0268973cdd24a75c48
ikkipower <piltzin1984@hotmail.com>
2013-06-01 13:57:21 +0200

    Adding first functions

---
+++ CRUD.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  CRUD.py
+#       
+#  Copyright 2013 Sergio Morlans <piltzin1984@gmail.com>
+#       
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#       
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#       
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
+from gi.repository import Gtk, GdkPixbuf, Gdk
+import os, sys
+
+class CRUD_GUI:
+
+    def __init__(self):
+
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("CRUD.glade")
+        self.handlers = {"OnDeleteWindow": Gtk.main_quit,}
+        self.builder.connect_signals(self.handlers)
+        self.window = self.builder.get_object("stwindow")
+        self.window.show_all()
+ 
+    def destroy(self,window):
+        Gtk.main_quit()
+    
+  
+    
+def main():
+    app = CRUD_GUI()
+    Gtk.main()
+
+if __name__ == "__main__":
+    sys.exit(main())
